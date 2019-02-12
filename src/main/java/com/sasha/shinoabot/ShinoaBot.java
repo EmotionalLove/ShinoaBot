2c04a96e78920c994e13315fa0ad7f4fbd5c3337
EmotionalLove <sashwho@vivaldi.net>
2019-02-12 00:05:39 -0800

    Initial commit

---
+++ src/main/java/com/sasha/shinoabot/ShinoaBot.java
@@ -0,0 +1,39 @@
+package com.sasha.shinoabot;
+
+import com.sasha.reminecraft.api.RePlugin;
+import com.sasha.simplecmdsys.SimpleCommandProcessor;
+
+public class ShinoaBot extends RePlugin {
+
+    public static final SimpleCommandProcessor BOT_CMD_PROC = new SimpleCommandProcessor(",");
+
+    @Override
+    public void onPluginInit() {
+        
+    }
+
+    @Override
+    public void onPluginEnable() {
+
+    }
+
+    @Override
+    public void onPluginDisable() {
+
+    }
+
+    @Override
+    public void onPluginShutdown() {
+
+    }
+
+    @Override
+    public void registerCommands() {
+
+    }
+
+    @Override
+    public void registerConfig() {
+
+    }
+}
