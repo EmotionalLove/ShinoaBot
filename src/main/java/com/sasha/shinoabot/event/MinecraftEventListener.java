f5dfc1acd99d01229ace73210db648cc1bf6e011
EmotionalLove <totsuka.sama@gmail.com>
2019-02-13 09:29:33 -0800

    comads yipee

--- src/main/java/com/sasha/shinoabot/event/MinecraftEventListener.java
+++ src/main/java/com/sasha/shinoabot/event/MinecraftEventListener.java
@@ -3,6 +3,7 @@ package com.sasha.shinoabot.event;
 import com.sasha.eventsys.SimpleEventHandler;
 import com.sasha.eventsys.SimpleListener;
 import com.sasha.reminecraft.api.event.ChatReceivedEvent;
+import com.sasha.shinoabot.ShinoaBot;
 
 public class MinecraftEventListener implements SimpleListener {
 
@@ -13,6 +14,8 @@ public class MinecraftEventListener implements SimpleListener {
     @SimpleEventHandler
     public void onChatRecieved(ChatReceivedEvent event) {
         int index = event.getMessageText().indexOf(">") + 1;
+        String raw = event.getMessageText().substring(index).trim();
+        if (raw.startsWith(",")) ShinoaBot.BOT_CMD_PROC.processCommand(raw);
     }
 
 }
