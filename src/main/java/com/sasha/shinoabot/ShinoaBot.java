a413db11ff4caeafd88952a9afbb307439a55f7f
EmotionalLove <totsuka.sama@gmail.com>
2019-02-13 09:39:26 -0800

    puke

--- src/main/java/com/sasha/shinoabot/ShinoaBot.java
+++ src/main/java/com/sasha/shinoabot/ShinoaBot.java
@@ -4,6 +4,7 @@ import com.github.steveice10.mc.protocol.packet.ingame.client.ClientChatPacket;
 import com.sasha.reminecraft.ReMinecraft;
 import com.sasha.reminecraft.api.RePlugin;
 import com.sasha.shinoabot.command.AboutCommand;
+import com.sasha.shinoabot.event.MinecraftEventListener;
 import com.sasha.shinoabot.localisation.EnumLocale;
 import com.sasha.shinoabot.localisation.LocalisedResponseManager;
 import com.sasha.simplecmdsys.SimpleCommandProcessor;
@@ -18,6 +19,7 @@ public class ShinoaBot extends RePlugin {
         // register the LocalisedResponses when the plugin is first initialised, so that we can access them as soon as possible
         LANG_MANAGER.registerLocalisedResponse(EnumLocale.ENG, "shinoa.about", "ShinoaBot " + Constants.VERSION);
         LANG_MANAGER.registerLocalisedResponse(EnumLocale.ENG, "shinoa.about", "シノアBOT " + Constants.VERSION);
+        this.getReMinecraft().EVENT_BUS.registerListener(new MinecraftEventListener());
     }
 
     @Override
